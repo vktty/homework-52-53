8166da758a1c66887972e408ec0c20b6a8c07879
Kateryna <vlkvskatia@gmail.com>
2026-04-24 23:29:10 +0300

    board create fix

--- client/src/pages/create/CreateBoard.tsx
+++ client/src/pages/create/CreateBoard.tsx
@@ -2,78 +2,101 @@ import { Form, Button, Input, Flex } from 'antd';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
-import { toastSuccess } from '../../context';
+import { toastError, toastSuccess } from '../../context';
 import './style.scss';
+import { useCreateBoardMutation } from '../../store/boards';
+import { getError } from '../../components/error';
 
 export const CreateBoard = () => {
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const [submittable, setSubmittable] = useState(false);
-    const values = Form.useWatch([], form);
+	const [form] = Form.useForm();
+	const navigate = useNavigate();
+	const [submittable, setSubmittable] = useState(false);
+	const values = Form.useWatch([], form);
+	const [submitBoard, addBoard] = useCreateBoardMutation();
 
-    useEffect(() => {
-        form.validateFields({ validateOnly: true })
-            .then(() => setSubmittable(true))
-            .catch(() => setSubmittable(false));
-    }, [form, values]);
+	useEffect(() => {
+		form.validateFields({ validateOnly: true })
+			.then(() => setSubmittable(true))
+			.catch(() => setSubmittable(false));
+	}, [form, values]);
 
-    const handleCreate = () => {
-        toastSuccess('New board created!');
-        navigate(`/boards`);
-    };
+	const handleCancel = () => {
+		toastSuccess('Changes cancelled!');
+		navigate(`/boards`);
+	};
 
-    const handleCancel = () => {
-        toastSuccess('Сhanges cancelled!');
-        navigate(`/boards`);
-    };
+	const handleCreate = async () => {
+		try {
+			const values = await form.validateFields();
+			await submitBoard({
+				name: values.boardName,
+				description: values.boardDescription,
+			}).unwrap();
+		} catch (error) {
+			toastError(getError(error));
+		}
+	};
 
-    return (
-        <>
-            <Form form={form} className='form'>
-                <h3>Create new board</h3>
-                <Form.Item
-                    name={'boardName'}
-                    label={'Board name:'}
-                    layout='vertical'
-                    rules={[
-                        { required: true, message: 'Please input board name!' },
-                    ]}>
-                    <Input placeholder='Enter board name' />
-                </Form.Item>
-                <Form.Item
-                    name={'boardDescription'}
-                    label={'Board description:'}
-                    layout='vertical'
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input board description!',
-                        },
-                    ]}>
-                    <Input.TextArea
-                        autoSize={{ minRows: 3, maxRows: 6 }}
-                        placeholder='Enter board description'
-                    />
-                </Form.Item>
+	useEffect(() => {
+		if (addBoard.isSuccess) {
+			toastSuccess('New board created!');
+			navigate(`/boards`);
+		}
+	}, [addBoard.isSuccess]);
 
-                <Flex justify='space-around'>
-                    <Button
-                        color='orange'
-                        variant='outlined'
-                        size='large'
-                        onClick={handleCancel}>
-                        Cancel
-                    </Button>
-                    <Button
-                        color='orange'
-                        variant='solid'
-                        size='large'
-                        disabled={!submittable}
-                        onClick={handleCreate}>
-                        Create Board
-                    </Button>
-                </Flex>
-            </Form>
-        </>
-    );
+	return (
+		<>
+			<Form form={form} className='form'>
+				<h3>Create new board</h3>
+				<Form.Item
+					name={'boardName'}
+					label={'Board name:'}
+					layout='vertical'
+					rules={[
+						{
+							required: true,
+							message: 'Please input board name!',
+						},
+					]}>
+					<Input placeholder='Enter board name' />
+				</Form.Item>
+				<Form.Item
+					name={'boardDescription'}
+					label={'Board description:'}
+					layout='vertical'
+					rules={[
+						{
+							required: true,
+							message: 'Please input board description!',
+						},
+					]}>
+					<Input.TextArea
+						autoSize={{
+							minRows: 3,
+							maxRows: 6,
+						}}
+						placeholder='Enter board description'
+					/>
+				</Form.Item>
+
+				<Flex justify='space-around'>
+					<Button
+						color='orange'
+						variant='outlined'
+						size='large'
+						onClick={handleCancel}>
+						Cancel
+					</Button>
+					<Button
+						color='orange'
+						variant='solid'
+						size='large'
+						disabled={!submittable}
+						onClick={handleCreate}>
+						Create Board
+					</Button>
+				</Flex>
+			</Form>
+		</>
+	);
 };
--- client/src/pages/create/CreateTask.tsx
+++ client/src/pages/create/CreateTask.tsx
@@ -10,114 +10,119 @@ import { workflow } from '../../utils';
 import './style.scss';
 
 export const CreateTask = () => {
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const [submittable, setSubmittable] = useState(false);
-    const [submitTask, addTask] = useCreateTaskMutation();
-    const { data: boardsData } = useGetBoardsQuery();
-    const values = Form.useWatch([], form);
+	const [form] = Form.useForm();
+	const navigate = useNavigate();
+	const [submittable, setSubmittable] = useState(false);
+	const values = Form.useWatch([], form);
+	const [submitTask, addTask] = useCreateTaskMutation();
+	const { data: boardsData } = useGetBoardsQuery();
 
-    useEffect(() => {
-        form.validateFields({ validateOnly: true })
-            .then(() => setSubmittable(true))
-            .catch(() => setSubmittable(false));
-    }, [form, values]);
+	useEffect(() => {
+		form.validateFields({ validateOnly: true })
+			.then(() => setSubmittable(true))
+			.catch(() => setSubmittable(false));
+	}, [form, values]);
 
-    const handleCancel = () => {
-        toastSuccess('Changes cancelled!');
-        navigate(-1);
-    };
+	const handleCancel = () => {
+		toastSuccess('Changes cancelled!');
+		navigate(-1);
+	};
 
-    const handleCreate = async () => {
-        try {
-            const values = await form.validateFields();
-            await submitTask({
-                title: values.taskName,
-                description: values.taskDescription,
-                boardId: values.boardId!,
-                workflow: values.boardWorkflow,
-            }).unwrap();
-        } catch (error) {
-            toastError(getError(error));
-        }
-    };
+	const handleCreate = async () => {
+		try {
+			const values = await form.validateFields();
+			await submitTask({
+				title: values.taskName,
+				description: values.taskDescription,
+				boardId: values.boardId!,
+				workflow: values.boardWorkflow,
+			}).unwrap();
+		} catch (error) {
+			toastError(getError(error));
+		}
+	};
 
-    useEffect(() => {
-        if (addTask.isSuccess) {
-            toastSuccess('New task created!');
-            navigate(-1);
-        }
-    }, [addTask.isSuccess]);
+	useEffect(() => {
+		if (addTask.isSuccess) {
+			toastSuccess('New task created!');
+			navigate(-1);
+		}
+	}, [addTask.isSuccess]);
 
-    const getBoards = () => {
-        return boardsData?.data.map((board) => ({
-            label: board.name,
-            value: board.id,
-        }));
-    };
+	const getBoards = () => {
+		return boardsData?.data.map((board) => ({
+			label: board.name,
+			value: board.id,
+		}));
+	};
 
-    const workflowOptions = workflow.map((option) => ({
-        label: option.label,
-        value: option.code,
-    }));
+	const workflowOptions = workflow.map((option) => ({
+		label: option.label,
+		value: option.code,
+	}));
 
-    return (
-        <Form form={form} className='form'>
-            <h3>Create new task</h3>
+	return (
+		<Form form={form} className='form'>
+			<h3>Create new task</h3>
 
-            <Form.Item
-                name='boardId'
-                label='Board'
-                layout='vertical'
-                rules={[{ required: true }]}>
-                <Select options={getBoards()} placeholder='Select board' />
-            </Form.Item>
+			<Form.Item
+				name='boardId'
+				label='Board'
+				layout='vertical'
+				rules={[{ required: true }]}>
+				<Select
+					options={getBoards()}
+					placeholder='Select board'
+				/>
+			</Form.Item>
 
-            <Form.Item
-                name='boardWorkflow'
-                label='Workflow'
-                layout='vertical'
-                rules={[{ required: true }]}>
-                <Select
-                    options={workflowOptions}
-                    placeholder='Select workflow'
-                />
-            </Form.Item>
+			<Form.Item
+				name='boardWorkflow'
+				label='Workflow'
+				layout='vertical'
+				rules={[{ required: true }]}>
+				<Select
+					options={workflowOptions}
+					placeholder='Select workflow'
+				/>
+			</Form.Item>
 
-            <Form.Item
-                name='taskName'
-                label='Task name'
-                layout='vertical'
-                rules={[{ required: true }]}>
-                <Input placeholder='Enter task name' />
-            </Form.Item>
+			<Form.Item
+				name='taskName'
+				label='Task name'
+				layout='vertical'
+				rules={[{ required: true }]}>
+				<Input placeholder='Enter task name' />
+			</Form.Item>
 
-            <Form.Item
-                name='taskDescription'
-                label='Task description'
-                layout='vertical'
-                rules={[{ required: true }]}>
-                <Input.TextArea autoSize={{ minRows: 3, maxRows: 6 }} />
-            </Form.Item>
+			<Form.Item
+				name='taskDescription'
+				label='Task description'
+				layout='vertical'
+				rules={[{ required: true }]}>
+				<Input.TextArea
+					autoSize={{ minRows: 3, maxRows: 6 }}
+				/>
+			</Form.Item>
 
-            <Flex justify='space-around'>
-                <Button
-                    color='orange'
-                    variant='outlined'
-                    size='large'
-                    onClick={handleCancel}>
-                    Cancel
-                </Button>
+			<Flex justify='space-around'>
+				<Button
+					color='orange'
+					variant='outlined'
+					size='large'
+					onClick={handleCancel}>
+					Cancel
+				</Button>
 
-                <Button
-                    color='orange'
-                    variant='solid'
-                    size='large'
-                    disabled={!submittable}
-                    onClick={handleCreate}>
-                    Create Task
-                </Button>
-            </Flex>
-        </Form>
-    );
+				<Button
+					color='orange'
+					variant='solid'
+					size='large'
+					disabled={!submittable}
+					onClick={handleCreate}>
+					Create Task
+				</Button>
+			</Flex>
+		</Form>
+	);
 };
